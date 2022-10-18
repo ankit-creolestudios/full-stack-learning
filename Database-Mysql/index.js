1af846ab33c027e3cba22f55bfdf8e9d671ce85b
senilshah <senil@creolestudios.com>
2022-10-18 09:07:38 +0530

    changes

---
+++ Database-Mysql/index.js
@@ -0,0 +1,8 @@
+{
+    grant_type:password,
+    client_id:sstudios-apigateway
+    username:ankit.kumar+test1490@creolestudios.com
+    password:@Ankitkn1234
+}
+
+added body data in x-www-form-urlencoded in postman
